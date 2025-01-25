9b2052bec06ac1cd75bbb9cbdbe57e638e264d28
Mostafa Salem <mostafa.salem.ext@siemens.com>
2025-01-25 23:55:11 +0200

    feat: added Main print lines for displaying number of wins, losses, ties against an opponent with only ROCK choice

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+import core.GameController
+import org.example.core.ChoiceGenerator
+import org.example.enums.Choice
+import org.example.model.GameStats
+
 fun main() {
-    println("Hello World!")
+    val gameController = GameController(ChoiceGenerator())
+    val gameResults = gameController.playCustomGames(100, Choice.ROCK)
+    val gameStats = GameStats(gameResults)
+
+    println("Player A wins ${gameStats.getNumWins()} of 100 games")
+    println("Player B wins ${gameStats.getNumLosses()} of 100 games")
+    println("Draws: ${gameStats.getNumTies()} of 100 games")
 }
